a811b0dd1884f37887e681c5ce5de2f4ae432dca
Saul Blanco Tejero <saul.blanco@sngular.com>
2023-11-07 11:44:36 +0100

    feat: add clothing endpoint with dummy data

---
+++ src/pages/api/clothes.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import clothesData from '../../data/clothes.json';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+  setTimeout(() => {
+    res.status(200).json(clothesData);
+  }, Math.random() * 2000);
+}
