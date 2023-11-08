bf1f6939730378744352d94e74a26ae577b2360e
Saul Blanco Tejero <saul.blanco@sngular.com>
2023-11-08 12:45:22 +0100

    chore: add unit tests for product component

---
+++ src/components/Product/Product.test.tsx
@@ -0,0 +1,61 @@
+import { render, screen } from '@testing-library/react';
+import ProductItem from './Product';
+import { Product } from 'models/product';
+
+jest.mock('../../lib/money', () => ({
+    formatEur: jest.fn((price: number) => `${price},00 €`),
+    getOfferObject: jest.fn((price: number, discount: number) => ({
+        previousPrice: discount && `${price},00 €`,
+        currentPrice: `${price - discount},00 €`,
+        discountPercentage: discount && `${discount}%`,
+    })),
+}));
+
+const product: Product = {
+    id: 0,
+    name: 'Test Product',
+    image: '/test-image.jpg',
+    price: 100,
+    discount: 10,
+    hasMoreColors: true,
+};
+
+describe('ProductItem', () => {
+    it('renders product name', () => {
+        render(<ProductItem product={product} />);
+        const productName = screen.getByTestId('product-name');
+        expect(productName).toHaveTextContent('Test Product');
+    });
+
+    it('renders product price with discount', () => {
+        render(<ProductItem product={product} />);
+        const productPrice = screen.getByTestId('product-current-offer');
+        expect(productPrice).toHaveTextContent('90,00 € (10%)');
+    });
+
+    it('renders product price without discount', () => {
+        const productWithoutDiscount: Product = {
+            ...product,
+            discount: 0,
+        };
+        render(<ProductItem product={productWithoutDiscount} />);
+        const productPrice = screen.getByTestId('product-price');
+        expect(productPrice).toHaveTextContent('100,00 €');
+    });
+
+    it('renders "más colores" when product has more colors', () => {
+        render(<ProductItem product={product} />);
+        const moreColors = screen.getByTestId('product-more-colors');
+        expect(moreColors).toBeInTheDocument();
+    });
+
+    it('does not render "más colores" when product does not have more colors', () => {
+        const productWithoutMoreColors: Product = {
+            ...product,
+            hasMoreColors: false,
+        };
+        render(<ProductItem product={productWithoutMoreColors} />);
+        const moreColors = screen.queryByTestId('product-more-colors');
+        expect(moreColors).not.toBeInTheDocument();
+    });
+});
--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -2,7 +2,7 @@ import { Product } from 'models/product';
 import Image from 'next/image';
 
 import styles from './Product.module.css';
-import { formatEur, getOfferObject } from 'lib/money';
+import { formatEur, getOfferObject } from '../../lib/money';
 import { useMemo } from 'react';
 
 type Props = {
@@ -14,23 +14,23 @@ export default function ProductItem({ product }: Props) {
   const offer = useMemo(() => getOfferObject(price, discount), [price, discount]);
 
   return (
-    <article className={styles.product}>
-      <figure>
+    <article className={styles.product} data-testid="product-item">
+      <figure data-testid="product-image">
         <Image src={image} alt={name} width={300} height={300} objectFit='cover' />
-        <figcaption className={styles.title}>{name}</figcaption>
+        <figcaption className={styles.title} data-testid="product-name">{name}</figcaption>
       </figure>
       {offer.discountPercentage ? (
-        <div>
-          <p className={styles['previous-price']}>{offer.previousPrice}</p>
-          <p className={styles['current-offer']}>
+        <div data-testid="product-offer">
+          <p className={styles['previous-price']} data-testid="product-previous-price">{offer.previousPrice}</p>
+          <p className={styles['current-offer']} data-testid="product-current-offer">
             {offer.currentPrice} ({offer.discountPercentage})
           </p>
         </div>
       ) : (
-        <p className={styles['price']}>{formatEur(price)}</p>
+        <p className={styles['price']} data-testid="product-price">{formatEur(price)}</p>
       )}
-      {hasMoreColors && <p className={styles['more-colors']}>más colores</p>}
-      <button className={styles['button-add']} type="button">
+      {hasMoreColors && <p className={styles['more-colors']} data-testid="product-more-colors">más colores</p>}
+      <button className={styles['button-add']} type="button" data-testid="product-add-button">
         Añadir
       </button>
     </article>
