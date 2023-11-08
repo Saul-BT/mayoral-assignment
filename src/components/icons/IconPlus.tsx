fb34030fadea5274564a1d1c524b8ccd15b8a377
Saul Blanco Tejero <saul.blanco@sngular.com>
2023-11-08 10:58:15 +0100

    chore: add necessary svg icons

---
+++ src/components/icons/IconPlus.tsx
@@ -0,0 +1,22 @@
+import { IconProps } from 'models/icon-props';
+
+export default function IconPlus({ size, strokeWidth = 2, ...props }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M12 5l0 14"></path>
+      <path d="M5 12l14 0"></path>
+    </svg>
+  );
+}
---
+++ src/components/icons/IconSearch.tsx
@@ -0,0 +1,22 @@
+import { IconProps } from 'models/icon-props';
+
+export default function IconSearch({ size, strokeWidth = 2, ...props }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+      <path d="M21 21l-6 -6"></path>
+    </svg>
+  );
+}
