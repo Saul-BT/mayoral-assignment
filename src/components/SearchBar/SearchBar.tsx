0fdbce03d7b6c1e97c68c8c5f3dca76b90af4e7e
Saul Blanco Tejero <saul.blanco@sngular.com>
2023-11-08 11:03:21 +0100

    feat: add debounced search & styles

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+import styles from './SearchBar.module.css';
+import IconSearch from 'components/icons/IconSearch';
 
 type Props = {
   onSearch: (search: string) => void;
 };
 
-export default function SearchBar(props: Props) {
-  const { onSearch } = props;
+export default function SearchBar({ onSearch }: Props) {
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onSearch(search);
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [search, onSearch]);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
   };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      onSearch(search);
-    }
-  };
-
   return (
-    <div className="search-bar">
-      <input
-        type="text"
-        placeholder="Search for a product"
-        value={search}
-        onChange={handleSearch}
-        onKeyDown={handleKeyDown}
-      />
-      <button type="button" onClick={() => onSearch(search)}>
-        Search
-      </button>
+    <div className={styles['search-bar']}>
+      <IconSearch className={styles['search-icon']} size={20} />
+      <input type="search" placeholder="Buscar" value={search} onChange={handleSearch} />
     </div>
   );
 }
