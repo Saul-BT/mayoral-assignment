faaf007a83e2a77821f88747fcbb7d2b07307022
Saul Blanco Tejero <saul.blanco@sngular.com>
2023-11-13 11:31:57 +0100

    chore: extract placeholder image constant

--- src/components/ProductGrid/Product/Product.tsx
+++ src/components/ProductGrid/Product/Product.tsx
@@ -2,15 +2,13 @@ import { formatEur, getOfferObject } from '../../../lib/money';
 
 import Image from 'next/image';
 import { Product } from 'models/product';
+import { placeholderTShirtImage } from 'utils/constants';
 import styles from './Product.module.css';
 import { useMemo } from 'react';
 
 type Props = {
   product: Product;
 };
-
-const placeholderImage =
-  'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMDAiIGhlaWdodD0iMzAwIiB2aWV3Qm94PSIwIDAgMjQgMjQiIHN0cm9rZS13aWR0aD0iMSIgc3Ryb2tlPSJjdXJyZW50Q29sb3IiIGZpbGw9Im5vbmUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCI+CiAgIDxwYXRoIHN0cm9rZT0ibm9uZSIgZD0iTTAgMGgyNHYyNEgweiIgZmlsbD0iI2ViZWJlYiI+PC9wYXRoPgogICA8cGF0aCBkPSJNMTUgNGw2IDJ2NWgtM3Y4YTEgMSAwIDAgMSAtMSAxaC0xMGExIDEgMCAwIDEgLTEgLTF2LThoLTN2LTVsNiAtMmEzIDMgMCAwIDAgNiAwIj48L3BhdGg+Cjwvc3ZnPg==';
 
 export default function ProductItem({ product }: Props) {
   const { name, image, price, discount, hasMoreColors } = product;
@@ -26,7 +24,7 @@ export default function ProductItem({ product }: Props) {
           height={300}
           objectFit="cover"
           placeholder="blur"
-          blurDataURL={placeholderImage}
+          blurDataURL={placeholderTShirtImage}
         />
         <figcaption className={styles.title} data-testid="product-name">
           {name}
